2708cec561c1b486803c4d068d9526d565eccc4e
Ali - Abu Hussein <lhtb9659@gmail.com>
2025-01-01 03:07:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import logging
+import logging, pyfiglet
 from tools.email_sender import EmailSender
 from tools.file_loader import FileLoader
 from tools.time_data import TimeData
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+
+TOOL_NAME = "MailVortex"
+TOOL_LOGO = pyfiglet.figlet_format(TOOL_NAME)
+
+GITHUB_ACCOUNT = "https://github.com/AbuH5"
 
 logging.basicConfig(
     filename="logs/app.log",
@@ -14,6 +22,11 @@ def display_time():
     return time_data.get_current_time()
 
 def main():
+    print(Fore.CYAN + TOOL_LOGO)
+    print(Fore.GREEN + f"{TOOL_NAME} is running...\n")
+
+    print(Fore.YELLOW + f"Check out the GitHub repository: {GITHUB_ACCOUNT}\n")
+
     try:
         credentials = FileLoader.load_json("config/credentials.json")
         email_details = FileLoader.load_json("config/email_details.json")
@@ -31,13 +44,13 @@ def main():
             try:
                 email_sender.send_email(recipient, subject, message)
                 logging.info(f"{current_time} - Email sent to {recipient}")
-                print(f"{current_time} - Email successfully sent to {recipient}")
+                print(Fore.GREEN + f"{current_time} - Email successfully sent to {recipient}")
             except Exception as e:
                 logging.error(f"{current_time} - Failed to send email to {recipient}: {e}")
-                print(f"{current_time} - Failed to send email to {recipient}")
+                print(Fore.RED + f"{current_time} - Failed to send email to {recipient}")
     except Exception as e:
         logging.critical(f"{display_time()} - Critical error occurred: {e}")
-        print("An error occurred. Check the logs for details.")
+        print(Fore.RED + "An error occurred. Check the logs for details.")
 
 if __name__ == "__main__":
     main()
